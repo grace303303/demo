05ef5c8c1b9f527b16f7f25a195a37a0ab48a3d2
grace303303 <39275944+grace303303@users.noreply.github.com>
2019-06-11 17:10:26 -0700

    Store data in browser

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 //jshint esversion: 6
 
 function submitFunction() {
-  var greetingsValue = document.getElementById("Greetings").value;
-  var contentValue = document.getElementById("Content").value.replace(/(?:\r\n|\r|\n)/g, '<br>');
-  var linkValue = "https://" + document.getElementById("buttonLink").value;
-  document.getElementById("changeGreetings").innerHTML = greetingsValue;
-  document.getElementById("changeContent").innerHTML = contentValue;
-  document.getElementById("changeLink").href = linkValue;
-  console.log(linkValue);
+
+document.getElementById("changeGreetings").innerHTML = document.getElementById("Greetings").value;
+document.getElementById("changeContent").innerHTML = document.getElementById("Content").value.replace(/(?:\r\n|\r|\n)/g, '<br>');
+document.getElementById("changeLink").href = "https://" + document.getElementById("buttonLink").value;
+
+
+if (typeof(Storage) !== "undefined") {
+// Store
+  localStorage.setItem("greetingsValue", document.getElementById("Greetings").value);
+  localStorage.setItem("contentValue", document.getElementById("Content").value.replace(/(?:\r\n|\r|\n)/g, '<br>'));
+  localStorage.setItem("linkValue", "https://" + document.getElementById("buttonLink").value)
+  
 }
+  
+}
+
+  // Retrieve
+  document.getElementById("changeGreetings").innerHTML = localStorage.getItem("greetingsValue");
+  document.getElementById("changeContent").innerHTML = localStorage.getItem("contentValue");
+  document.getElementById("changeLink").href = localStorage.getItem("linkValue");
